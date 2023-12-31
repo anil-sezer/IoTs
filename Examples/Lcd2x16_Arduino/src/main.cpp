0965331166fae691f5b21f4e3073301b9b53bb64
Anıl Sezer <msnanil@hotmail.com>
2023-12-31 12:29:51 +0300

    feat: LiquidCrystal_I2C tested & ready

--- Examples/Lcd2x16_Arduino/src/main.cpp
+++ Examples/Lcd2x16_Arduino/src/main.cpp
@@ -1,18 +1,24 @@
 #include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// https://lastminuteengineers.com/i2c-lcd-arduino-tutorial/
+// The first screen I got is at: 0x27
+
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x3F for a 16 chars and 2 line display
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  lcd.init();
+  lcd.clear();         
+  lcd.backlight();      // Make sure backlight is on
+  
+  
+  // Print a message on both lines of the LCD.
+  lcd.setCursor(0,0);   //Set cursor to character 2 on line 0
+  lcd.print("Cur. Temp: 19.02");
+  
+  lcd.setCursor(0,1);   //Move cursor to character 2 on line 1
+  lcd.print("Tar. Temp: 22.00");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }

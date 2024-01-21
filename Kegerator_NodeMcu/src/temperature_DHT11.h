fa8b87f8455bdffca3160bd5b048dc2f91aeb292
Anıl Sezer <msnanil@hotmail.com>
2024-01-21 14:01:02 +0300

    feat: Added DHT11 sensor adapter code

---
+++ Kegerator_NodeMcu/src/temperature_DHT11.h
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+// DHT Temperature & Humidity Sensor
+// Unified Sensor Library Example
+// Written by Tony DiCola for Adafruit Industries
+// Released under an MIT license.
+
+// REQUIRES the following Arduino libraries:
+// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
+// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
+
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+void initializeTemperatureAndMoistureSensor();
+std::array<float, 2> getTempAndHumidity();
+
+extern uint32_t sensorMinimumDelay;
